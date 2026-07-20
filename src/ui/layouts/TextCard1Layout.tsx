ef5b7abccfe12c05bb06de0ba82fae3ceb07d1cd
divyanshchahar <divyanshchahar@outlook.com>
2026-07-20 08:08:41 +0530

    added text card 1

---
+++ src/ui/layouts/TextCard1Layout.tsx
@@ -0,0 +1,51 @@
+import styles from "./TextCard1Layout.module.css"
+
+export interface TextCard1LayoutPropTypes {
+    lineColor: "saffron" | "blue" | "green",
+    backgroundColor: "background" | "foreground",
+    headingText: string,
+    bodyText: string,
+    colorScheme: string,
+    addedStyles?: string,
+}
+
+
+export default function TextCard1Layout({
+                                            lineColor,
+                                            backgroundColor,
+                                            headingText,
+                                            bodyText,
+                                            colorScheme
+                                        }: TextCard1LayoutPropTypes) {
+    let selectedLineColor = ""
+    let seletcedBackgroundColor = ""
+
+    switch (lineColor) {
+        case "saffron":
+            selectedLineColor = styles.saffron
+            break;
+        case "blue":
+            selectedLineColor = styles.blue
+            break;
+        case "green":
+            selectedLineColor = styles.green
+            break;
+    }
+
+    switch (backgroundColor) {
+        case "background":
+            seletcedBackgroundColor = styles.background
+            break;
+        case "foreground":
+            seletcedBackgroundColor = styles.foreground
+            break;
+    }
+
+    return (
+        <div className={`${styles.container} ${selectedLineColor} ${seletcedBackgroundColor} ${colorScheme}`}>
+            <hr/>
+            <p className={"h6"}>{headingText}</p>
+            <p className={"lightNormal"}>{bodyText}</p>
+        </div>
+    )
+}
