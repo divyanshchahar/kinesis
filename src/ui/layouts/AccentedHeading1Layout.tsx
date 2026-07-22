3f06ae5b3a4b1d4477e02ea6d87b465b7d0d9d8e
divyanshchahar <divyanshchahar@outlook.com>
2026-07-22 15:39:47 +0530

    added new layout

---
+++ src/ui/layouts/AccentedHeading1Layout.tsx
@@ -0,0 +1,42 @@
+import styles from "@/ui/layouts/AccentedHeading1Layout.module.css";
+
+export interface AccentedHeading1LayoutPropTypes {
+    colorScheme: string,
+    headingText: string
+    addedStyle?: string,
+    version: "saffron" | "blue" | "green"
+}
+
+export default function AccentedHeading1Layout({
+                                                   colorScheme,
+                                                   headingText,
+                                                   version,
+                                                   addedStyle
+                                               }: AccentedHeading1LayoutPropTypes) {
+
+    let selectedColor = ""
+
+
+    switch (version) {
+        case "green":
+            selectedColor = styles.green
+            break;
+
+        case "blue":
+            selectedColor = styles.blue
+            break;
+
+        case "saffron":
+            selectedColor = styles.saffron
+            break;
+    }
+
+    return <div className={`${colorScheme} ${styles.container} ${addedStyle}`}>
+        <div className={`${styles.container}`}>
+            <div className={`${styles.squareContainer}`}>
+                <div className={`${selectedColor} ${styles.sqaure}`}></div>
+            </div>
+            <p className={`h2`}>{headingText}</p>
+        </div>
+    </div>
+}
