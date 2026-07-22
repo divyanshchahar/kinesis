2b4bf00fffcdecaaa2c9b27ae0451ca1d1a746c3
divyanshchahar <divyanshchahar@outlook.com>
2026-07-22 17:40:28 +0530

    added new layout

---
+++ src/ui/layouts/Header2Layout.tsx
@@ -0,0 +1,64 @@
+import styles from "./Header2Layout.module.css"
+
+export interface Header2LayoutPropTypes {
+    colorScheme: string,
+    capsulText: string,
+    headingText: string,
+    bodyText: string,
+    hasBox: true | false
+    boxText?: string,
+    version: "saffron" | "green" | "blue"
+}
+
+export default function Header2Layout({
+                                          colorScheme,
+                                          capsulText,
+                                          headingText,
+                                          bodyText,
+                                          hasBox,
+                                          boxText,
+                                          version
+                                      }:
+                                      Header2LayoutPropTypes
+) {
+
+    let selectedColor = ""
+    let selctedColorBox = ""
+
+    switch (version) {
+        case "saffron":
+            selctedColorBox = styles.saffronBox
+            selectedColor = styles.saffron
+            break;
+        case "green":
+            selctedColorBox = styles.greenBox
+            selectedColor = styles.green
+            break;
+        case "blue":
+            selctedColorBox = styles.blueBox
+            selectedColor = styles.blue
+            break;
+
+
+    }
+    return (
+        <div className={`${styles.container} ${colorScheme} paddedContainer`}>
+            <div className={`${styles.top} lightNormal`}>
+                All products
+                <div className={`${styles.capsule} ${selectedColor} regularNormal`}>
+                    {capsulText}
+                </div>
+            </div>
+            <p className={`h1 ${styles.heading}`}>{headingText}</p>
+            <p>{bodyText}</p>
+
+            {
+                hasBox && <div className={`${styles.box} ${colorScheme} ${selctedColorBox}`}>
+                    {
+                        boxText
+                    }
+                </div>
+            }
+        </div>
+    )
+}
