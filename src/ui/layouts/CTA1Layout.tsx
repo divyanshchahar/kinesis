a24aa902f6facbfd6bb753b870ad264e80f24e3f
divyanshchahar <divyanshchahar@outlook.com>
2026-07-27 10:49:56 +0530

    updated styling

--- src/ui/layouts/CTA1Layout.tsx
+++ src/ui/layouts/CTA1Layout.tsx
@@ -11,7 +11,7 @@ export interface CTALayoutProptype {
 export default function CTA1Layout({colorScheme, addedStyle, bodyText, headingText}: CTALayoutProptype) {
     return (
         <div className={`paddedContainer ${colorScheme} ${styles.container} ${addedStyle}`}>
-            <div className={`maxWidthContainer ${styles.flex}`}>
+            <div className={`${styles.flex}`}>
                 <p className={`h2 ${styles.heading}`}>{headingText}</p>
                 <p className={`lightNormal`}>{bodyText}</p>
                 <CTAButton version={"large"} ctaText={"Get In Touch"} addedStyle={styles.fitted}/>
