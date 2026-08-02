754f08def3885bb3cc5ccb33127e1ff75cb8c2f2
divyanshchahar <divyanshchahar@outlook.com>
2026-08-02 22:51:18 +0530

    added new layout

--- src/ui/layouts/CTA1Layout.tsx
+++ src/ui/layouts/CTA1Layout.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import styles from "./CTA1Layout.module.css"
 import CTAButton from "@/ui/componenets/CTAButton";
+import {useRouter} from "next/navigation";
+import internalLinks from "./../../const/internalRoutes.json"
 
 export interface CTALayoutProptype {
     colorScheme: string,
@@ -9,12 +13,17 @@ export interface CTALayoutProptype {
 }
 
 export default function CTA1Layout({colorScheme, addedStyle, bodyText, headingText}: CTALayoutProptype) {
+
+    const router = useRouter();
+
     return (
         <div className={`paddedContainer ${colorScheme} ${styles.container} ${addedStyle}`}>
             <div className={`${styles.flex}`}>
                 <p className={`h2 ${styles.heading}`}>{headingText}</p>
                 <p className={`lightNormal`}>{bodyText}</p>
-                <CTAButton version={"large"} ctaText={"Get In Touch"} addedStyle={styles.fitted}/>
+                <CTAButton version={"large"} ctaText={"Get In Touch"} addedStyle={styles.fitted} clickHandler={() => {
+                    router.push(internalLinks.contactus)
+                }}/>
             </div>
         </div>
     )
